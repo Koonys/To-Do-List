8a33cbab04388b023ed8facba999fbd730164af8
SeokHoon <sughoon1@gmail.com>
2024-02-16 01:37:45 +0900

    드래그앤드랍 적용

--- main.js
+++ main.js
@@ -24,7 +24,6 @@ for(let i=0;i<tabs.length;i++){
         filter(e)
     });
 }
-console.log(tabs);
 
 addButton.addEventListener("click",addTask);
 taskInput.addEventListener("keydown",function(e){
@@ -64,12 +63,12 @@ function render(){
     }
     for(let i=0;i<list.length;i++){
         if(list[i].isComplete==true){
-            resultHTML+=`<div class="task"><div id="${list[i].id}" class="task-done task-content">${list[i].taskContent}</div><div class="task-button"><button class="btn btn-light" onclick="modi('${list[i].id}')" disabled><i class="fa-regular fa-pen-to-square"></i></button></button></button><button class="btn btn-light return" onclick="toggleComplete('${list[i].id}')"><i class="fa-solid fa-rotate-left"></i></button><button class="btn btn-light delete" onclick="deleteTask('${list[i].id}')"><i class="fa-solid fa-eraser"></i></button></div></div>`;
+            resultHTML+=`<div class="task" id="${list[i].id}" draggable="true"><div class="task-done task-content">${list[i].taskContent}</div><div class="task-button"><button class="btn btn-light" onclick="modi('${list[i].id}')" disabled><i class="fa-regular fa-pen-to-square"></i></button></button></button><button class="btn btn-light return" onclick="toggleComplete('${list[i].id}')"><i class="fa-solid fa-rotate-left"></i></button><button class="btn btn-light delete" onclick="deleteTask('${list[i].id}')"><i class="fa-solid fa-eraser"></i></button></div></div>`;
         }else{
-            resultHTML += `<div class="task"><div id="${list[i].id}" class="task-content">${list[i].taskContent}</div><div class="task-button"><button class="btn btn-light" onclick="modi('${list[i].id}')"><i class="fa-regular fa-pen-to-square"></i></button><button class="btn btn-light check" onclick="toggleComplete('${list[i].id}')"><i class="fa-solid fa-circle-check"></i></button><button class="btn btn-light delete" onclick="deleteTask('${list[i].id}')"><i class="fa-solid fa-eraser"></i></button></div></div>`;
+            resultHTML += `<div class="task" id="${list[i].id}" draggable="true"><div class="task-content">${list[i].taskContent}</div><div class="task-button"><button class="btn btn-light" onclick="modi('${list[i].id}')"><i class="fa-regular fa-pen-to-square"></i></button><button class="btn btn-light check" onclick="toggleComplete('${list[i].id}')"><i class="fa-solid fa-circle-check"></i></button><button class="btn btn-light delete" onclick="deleteTask('${list[i].id}')"><i class="fa-solid fa-eraser"></i></button></div></div>`;
         }
     }
-    document.getElementById("task-board").innerHTML = resultHTML;
+    document.getElementById("sortable-list").innerHTML = resultHTML;
 }
 
 function toggleComplete(id){
@@ -136,6 +135,63 @@ function filter(e){
     }
     render();
 }
+
+const item = document.querySelector(".list");
+item.addEventListener("dragstart",(e)=>{
+    if(e.target.classList.contains("task")){
+        draggedTask = e.target;
+        e.target.classList.add("dragging");
+        e.dataTransfer.effectAllowed = "move";
+        e.dataTransfer.setData("text.plain",draggedTask.id);
+    }
+})
+
+item.addEventListener("dragend",(e)=>{
+    e.target.classList.remove("dragging");
+    draggedTask=null;
+
+    const newTaskList = [];
+    const tasks = item.querySelectorAll(".task");
+    tasks.forEach(function(task){
+        const taskId = task.getAttribute("id");
+        const taskOBJ = taskList.find(function(item){
+            return item.id===taskId
+        });
+        newTaskList.push(taskOBJ);
+    });
+    
+    taskList = newTaskList;
+    render();
+})
+
+item.addEventListener("dragover",function(e){
+    e.preventDefault();
+    if(e.target.classList.contains("task")){
+        const afterElement = getDragAfterElement(item, e.clientY);
+        console.log(afterElement);
+        const taskBeingDragged = document.getElementById(draggedTask.id);
+        if (afterElement == null) {
+            item.appendChild(taskBeingDragged);
+        } else {
+            item.insertBefore(taskBeingDragged, afterElement);
+        }
+    }
+});
+
+function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll('.task:not(.dragging)')];
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
+        if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child };
+        } else {
+            return closest;
+        }
+    }, { offset: Number.NEGATIVE_INFINITY, element: null }).element;
+}
+
+
 
 function randomId(){
     return '_' + Math.random().toString(36).substring(2,9);
